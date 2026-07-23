0e8a980637c1564bf2916d2ce0eb829365c0a7c5
Rodrigo Lizana <rodrigoalvearlizana@gmail.com>
2026-07-23 22:23:45 +0000

    hoy

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-07-23T20:36:21.716876777+00:00
+-- finished_at: 2026-07-23T20:36:23.743792709+00:00
+-- elapsed: 2.0s
+-- outcome: success
+-- dialect: bigquery
+-- node_id: not available
+-- query_id: 4RTwCsAdHeFU96M4HSSgPtSyr5d
+-- desc: not provided
+select 1 as id;
---
+++ target/.lsp/compiled/mi_proyecto_banco/models/example/my_second_dbt_model.sql
@@ -0,0 +1,6 @@
+
+-- Use the `ref` function to select from other models
+
+select *
+from `mi-proyecto-dbt-nube`.`ds_dbt_banco`.`my_first_dbt_model`
+where id = 1
